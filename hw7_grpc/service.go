96956a9a2d4ce1cf9b8636e14ddc83d28ebe6add
Alexey Kremsa <10kdmg@gmail.com>
2018-08-16 00:29:12 +0300

    Start grpc server

--- hw7_grpc/service.go
+++ hw7_grpc/service.go
@@ -1,4 +1,37 @@
 package main
 
-// тут вы пишете код
-// обращаю ваше внимание - в этом задании запрещены глобальные переменные
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func StartMyMicroservice(ctx context.Context, addr, acl string) error {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		panic(fmt.Sprintf("can not start the service. %s", err.Error()))
+	}
+
+	srv := grpc.NewServer()
+	fmt.Println("starting server at: ", addr)
+
+	go func() {
+		select {
+		case <-ctx.Done():
+			srv.Stop()
+			return
+		}
+	}()
+
+	go func() {
+		err := srv.Serve(lis)
+		if err != nil {
+			panic(err)
+		}
+		return
+	}()
+
+	return nil
+}
