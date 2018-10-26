521d795c40bd5297c9613e80952110329d44b562
Alexey <10kdmg@gmail.com>
2018-10-27 01:11:27 +0300

    hw7: fixed race conditions

--- hw7_grpc/service.go
+++ hw7_grpc/service.go
@@ -6,12 +6,132 @@ import (
 	"fmt"
 	math "math"
 	"net"
+	"strings"
 	"sync"
 	"time"
 
 	proto "github.com/golang/protobuf/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
 )
+
+const (
+	bizAdmin = "biz_admin"
+	bizUser  = "biz_user"
+	logger   = "logger"
+)
+
+func getConsumerNameFromContext(ctx context.Context) (string, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return "", grpc.Errorf(codes.Unauthenticated, "can not get metadata")
+	}
+	consumer, ok := md["consumer"]
+	if !ok || len(consumer) != 1 {
+		return "", grpc.Errorf(codes.Unauthenticated, "can not get metadata")
+	}
+
+	return consumer[0], nil
+}
+
+func (srv *service) checkBizPermission(consumer, method string) error {
+	allowedMethods, ok := srv.aclStorage[consumer]
+	if !ok {
+		return grpc.Errorf(codes.Unauthenticated, "permission denied")
+	}
+
+	for _, m := range allowedMethods {
+		//check if everything allowed
+		splitted := strings.Split(m, "/")
+		if len(splitted) == 3 && splitted[2] == "*" {
+			return nil
+		}
+
+		if m == method {
+			return nil
+		}
+	}
+
+	return grpc.Errorf(codes.Unauthenticated, "permission denied")
+}
+
+func parseACL(acl string) (map[string][]string, error) {
+	var aclParsed map[string]*json.RawMessage
+	result := make(map[string][]string)
+
+	err := json.Unmarshal([]byte(acl), &aclParsed)
+	if err != nil {
+		return nil, err
+	}
+
+	for k, v := range aclParsed {
+		var val []string
+		err := json.Unmarshal(*v, &val)
+		if err != nil {
+			return nil, err
+		}
+
+		result[k] = val
+	}
+
+	return result, nil
+}
+
+func (srv *service) addListener(l *listener) {
+	srv.m.Lock()
+	srv.listeners = append(srv.listeners, l)
+	srv.m.Unlock()
+}
+
+func (srv *service) logsSender() {
+	for {
+		select {
+		case log := <-srv.incomingLogsCh:
+			srv.m.RLock()
+			for _, l := range srv.listeners {
+				l.logsCh <- log
+			}
+			srv.m.RUnlock()
+
+		case <-srv.closeListenersCh:
+			srv.m.RLock()
+			for _, l := range srv.listeners {
+				l.closeCh <- struct{}{}
+			}
+			srv.m.RUnlock()
+
+			return
+		}
+	}
+}
+
+func (srv *service) statsSender() {
+	for {
+		select {
+		case statMsg := <-srv.incomingStatCh:
+			srv.m.RLock()
+			for _, l := range srv.statListeners {
+				l.statCh <- statMsg
+			}
+			srv.m.RUnlock()
+
+		case <-srv.closeStatListenersCh:
+			srv.m.RLock()
+			for _, l := range srv.statListeners {
+				l.closeCh <- struct{}{}
+			}
+			srv.m.RUnlock()
+			return
+		}
+	}
+}
+
+func (srv *service) addStatListener(sl *statListener) {
+	srv.m.Lock()
+	srv.statListeners = append(srv.statListeners, sl)
+	srv.m.Unlock()
+}
 
 func (s *service) Check(ctx context.Context, n *Nothing) (*Nothing, error) {
 	return &Nothing{}, nil
@@ -245,10 +365,11 @@ func (s *service) streamInterceptor(srv interface{},
 			consumerName: consumer,
 			methodName:   info.FullMethod,
 		}
-
+		s.m.RLock()
 		for _, l := range s.listeners {
 			l.logsCh <- &msg
 		}
+		s.m.RUnlock()
 
 	} else {
 		msg := statMsg{
@@ -256,10 +377,11 @@ func (s *service) streamInterceptor(srv interface{},
 			methodName:   info.FullMethod,
 		}
 
+		s.m.RLock()
 		for _, l := range s.statListeners {
 			l.statCh <- &msg
 		}
-
+		s.m.RUnlock()
 	}
 
 	return handler(srv, ss)
