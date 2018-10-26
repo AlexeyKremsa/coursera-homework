8f6bb442392481f6487a9aa397dc9b7692c0439b
Alexey <10kdmg@gmail.com>
2018-10-27 00:52:26 +0300

    hw7: prepeared to upload

--- hw7_grpc/biz-handler.go
+++ hw7_grpc/biz-handler.go
@@ -1,17 +1,1 @@
 package main
-
-import (
-	context "golang.org/x/net/context"
-)
-
-func (s *service) Check(ctx context.Context, n *Nothing) (*Nothing, error) {
-	return &Nothing{}, nil
-}
-
-func (s *service) Add(ctx context.Context, n *Nothing) (*Nothing, error) {
-	return &Nothing{}, nil
-}
-
-func (s *service) Test(ctx context.Context, n *Nothing) (*Nothing, error) {
-	return &Nothing{}, nil
-}
