8de9b15bb0354c135e5f1d7c71e3ade7bd7e78fb
Alexey <10kdmg@gmail.com>
2018-11-01 00:26:29 +0300

    hw8: all tests passed

--- hw8_i2s/i2s.go
+++ hw8_i2s/i2s.go
@@ -11,18 +11,33 @@ func i2s(data interface{}, out interface{}) error {
 	v := reflect.ValueOf(data)
 
 	vOut := reflect.ValueOf(out)
+
+	if vOut.Kind() != reflect.Ptr {
+		return errors.New("out parameter is not a poitner")
+	}
+
 	vOut = vOut.Elem()
+
 	if v.Kind() == reflect.Slice {
+		if vOut.Kind() != reflect.Slice {
+			return errors.New("expected slice as out parameter")
+		}
 		sliceItself := reflect.ValueOf(v.Interface())
 		outSlice := reflect.MakeSlice(vOut.Type(), sliceItself.Len(), sliceItself.Len())
 		for i := 0; i < v.Len(); i++ {
-			i2s(sliceItself.Index(i).Interface(), outSlice.Index(i).Addr().Interface())
+			err := i2s(sliceItself.Index(i).Interface(), outSlice.Index(i).Addr().Interface())
+			if err != nil {
+				return err
+			}
 		}
 		vOut.Set(outSlice)
-
 	}
 
 	if v.Kind() == reflect.Map {
+		if len(v.MapKeys()) == 0 {
+			return fmt.Errorf("empty map provided")
+		}
+
 		for _, key := range v.MapKeys() {
 			fieldVal := v.MapIndex(key)
 			fmt.Println("===", key.Interface(), fieldVal.Interface(), reflect.TypeOf(fieldVal.Interface()))
@@ -31,7 +46,10 @@ func i2s(data interface{}, out interface{}) error {
 			switch reflect.TypeOf(fieldVal.Interface()).Kind() {
 			case reflect.Map:
 				outVal := reflect.New(fOut.Type()).Elem()
-				i2s(fieldVal.Interface(), outVal.Addr().Interface())
+				err := i2s(fieldVal.Interface(), outVal.Addr().Interface())
+				if err != nil {
+					return err
+				}
 				fOut.Set(outVal)
 
 			case reflect.Float64:
@@ -39,19 +57,41 @@ func i2s(data interface{}, out interface{}) error {
 				if !ok {
 					return errors.New("can not cast to float64")
 				}
+				fmt.Println(fOut.Type().String())
+				if fOut.Type().String() != "int" {
+					return errors.New("can not set int value")
+				}
+
 				fOut.SetInt(int64(castVal))
 
 			case reflect.String:
-				fOut.SetString(fieldVal.Interface().(string))
+				val, ok := fieldVal.Interface().(string)
+				if !ok {
+					return errors.New("can not set string value")
+				}
+				if fOut.Type().String() != "string" {
+					return errors.New("can not set string value")
+				}
+				fOut.SetString(val)
 
 			case reflect.Bool:
-				fOut.SetBool(fieldVal.Interface().(bool))
+				val, ok := fieldVal.Interface().(bool)
+				if !ok {
+					return errors.New("can not set bool value")
+				}
+				if fOut.Type().String() != "bool" {
+					return errors.New("can not set string value")
+				}
+				fOut.SetBool(val)
 
 			case reflect.Slice:
 				sliceItself := reflect.ValueOf(fieldVal.Interface())
 				outSlice := reflect.MakeSlice(fOut.Type(), sliceItself.Len(), sliceItself.Len())
 				for i := 0; i < sliceItself.Len(); i++ {
-					i2s(sliceItself.Index(i).Interface(), outSlice.Index(i).Addr().Interface())
+					err := i2s(sliceItself.Index(i).Interface(), outSlice.Index(i).Addr().Interface())
+					if err != nil {
+						return err
+					}
 				}
 				fOut.Set(outSlice)
 			}
